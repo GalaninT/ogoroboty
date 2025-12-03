6aa6621539e3697ae32fbb3b6bebe7717f7729b8
Тимур Галанин <galanintim@yandex.ru>
2025-12-03 10:54:22 +0300

    Update robot_server.cpp

--- robot_server.cpp
+++ robot_server.cpp
@@ -1,111 +1,131 @@
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <unistd.h>
-#include <wiringSerial.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <termios.h>
 
-class RobotServer {
-private:
-    int server_fd, new_socket;
+// Настройки сервера
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+// Настройки Serial (для Arduino)
+#define SERIAL_PORT "/dev/ttyACM0"
+#define BAUD_RATE B9600
+
+int serial_fd;
+
+// Функция для открытия Serial-порта
+int openSerialPort(const char* port, speed_t baud_rate) {
+    int fd = open(port, O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1) {
+        perror("Ошибка открытия Serial-порта");
+        return -1;
+    }
+    
+    struct termios options;
+    tcgetattr(fd, &options);
+    cfsetispeed(&options, baud_rate);
+    cfsetospeed(&options, baud_rate);
+    
+    options.c_cflag |= (CLOCAL | CREAD);
+    options.c_cflag &= ~PARENB;
+    options.c_cflag &= ~CSTOPB;
+    options.c_cflag &= ~CSIZE;
+    options.c_cflag |= CS8;
+    options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    options.c_oflag &= ~OPOST;
+    
+    tcsetattr(fd, TCSANOW, &options);
+    return fd;
+}
+
+int main() {
+    std::cout << "Запуск TCP-сервера для OmegaBot..." << std::endl;
+
+    // 1. Открываем Serial-порт для Arduino
+    serial_fd = openSerialPort(SERIAL_PORT, BAUD_RATE);
+    if (serial_fd == -1) {
+        std::cerr << "Не удалось открыть Serial-порт" << std::endl;
+        return 1;
+    }
+    std::cout << "Serial-порт открыт: " << SERIAL_PORT << std::endl;
+
+    // 2. Создаем TCP-сервер
+    int server_fd;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    int arduino_port;
-    
-public:
-    RobotServer(int port) {
-        // Создание TCP сокета
-        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-            perror("socket failed");
-            exit(EXIT_FAILURE);
-        }
-        
-        // Настройка опций сокета
-        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-            perror("setsockopt");
-            exit(EXIT_FAILURE);
-        }
-        
-        address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY;
-        address.sin_port = htons(port);
-        
-        // Привязка сокета к порту
-        if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-            perror("bind failed");
-            exit(EXIT_FAILURE);
-        }
-        
-        // Подключение к Arduino
-        arduino_port = serialOpen("/dev/ttyACM0", 9600);
-        if (arduino_port < 0) {
-            std::cerr << "Unable to open serial connection to Arduino" << std::endl;
-            exit(EXIT_FAILURE);
-        }
-        
-        std::cout << "Connected to Arduino" << std::endl;
-    }
-    
-    void start() {
-        // Прослушивание входящих подключений
-        if (listen(server_fd, 3) < 0) {
-            perror("listen");
-            exit(EXIT_FAILURE);
-        }
-        
-        std::cout << "Server listening on port 8080..." << std::endl;
-        
-        while (true) {
-            // Принятие входящего подключения
-            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-                perror("accept");
-                exit(EXIT_FAILURE);
-            }
-            
-            handleClient(new_socket);
-            close(new_socket);
-        }
-    }
-    
-private:
-    void handleClient(int client_socket) {
-        char buffer[1024] = {0};
-        int valread;
-        
-        while ((valread = read(client_socket, buffer, 1024)) > 0) {
-            std::string command(buffer, valread);
-            std::cout << "Received command: " << command << std::endl;
-            
-            // Обработка команд и отправка на Arduino
-            processCommand(command);
-            
-            // Ответ клиенту
-            std::string response = "Command executed: " + command + "\n";
-            send(client_socket, response.c_str(), response.length(), 0);
-            
-            memset(buffer, 0, sizeof(buffer));
-        }
-    }
-    
-    void processCommand(const std::string& command) {
-        char arduino_command = 'S'; // По умолчанию - стоп
-        
-        if (command == "FORWARD" || command == "F") arduino_command = 'F';
-        else if (command == "BACKWARD" || command == "B") arduino_command = 'B';
-        else if (command == "LEFT" || command == "L") arduino_command = 'L';
-        else if (command == "RIGHT" || command == "R") arduino_command = 'R';
-        else if (command == "STOP" || command == "S") arduino_command = 'S';
-        
-        // Отправка команды на Arduino
-        serialPutchar(arduino_port, arduino_command);
-        std::cout << "Sent to Arduino: " << arduino_command << std::endl;
-    }
-};
+    char buffer[BUFFER_SIZE] = {0};
 
-int main() {
-    RobotServer server(8080);
-    server.start();
+    // Создаем сокет
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Ошибка создания сокета");
+        return 1;
+    }
+
+    // Настройка опций сокета
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("Ошибка настройки сокета");
+        return 1;
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Привязка сокета
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Ошибка привязки сокета");
+        return 1;
+    }
+
+    // Прослушивание порта
+    if (listen(server_fd, 3) < 0) {
+        perror("Ошибка прослушивания");
+        return 1;
+    }
+
+    std::cout << "Сервер запущен. Ожидание подключения на порту " << PORT << "..." << std::endl;
+
+    // 3. Принимаем подключение
+    int client_fd;
+    if ((client_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("Ошибка принятия подключения");
+        return 1;
+    }
+
+    std::cout << "Клиент подключен!" << std::endl;
+
+    // 4. Основной цикл обработки команд
+    while (true) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int valread = read(client_fd, buffer, BUFFER_SIZE);
+        
+        if (valread <= 0) {
+            std::cout << "Клиент отключился" << std::endl;
+            break;
+        }
+
+        // Полученная команда
+        char command = buffer[0];
+        std::cout << "Получена команда: '" << command << "'" << std::endl;
+
+        // Отправляем команду в Arduino
+        if (write(serial_fd, &command, 1) == -1) {
+            perror("Ошибка отправки в Serial");
+        }
+
+        // Эхо-ответ клиенту
+        send(client_fd, buffer, valread, 0);
+    }
+
+    // Закрываем соединения
+    close(client_fd);
+    close(server_fd);
+    close(serial_fd);
+    
+    std::cout << "Сервер остановлен" << std::endl;
     return 0;
 }
